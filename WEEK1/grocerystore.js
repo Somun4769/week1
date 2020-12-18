e72f82217da53372df397e3917ce1b6d7295400f
Somun4769 <71671219+Somun4769@users.noreply.github.com>
2020-12-18 16:05:41 -0700

    Add files via upload

---
+++ WEEK1/grocerystore.js
@@ -0,0 +1,13 @@
+var shopper =
+{
+   storeName: "Walmart",
+   storeLocation: "Mesa, AZ",
+   shoppingType:"vegetarian",
+   shoppingCart:["Pickles, beer, sandwich meat, bread"],
+   displayInfo:function(){
+console.log("${shopper.store")
+   }
+
+}
+
+ 
